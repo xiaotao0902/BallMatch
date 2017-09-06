692d6f964ee791c92dcdd8593d4c05dcc035c916
sven <dllmyliu@cn.ibm.com>
2017-09-06 18:16:21 +0800

    add code 0906

---
+++ BallMatchService/src/main/webapp/js/main.js
@@ -0,0 +1,9 @@
+var playerJson=[{name:"Lu Dong",level:"7.23",achievement:3,
+					winPercent:"45%",club:"美洲豹P3",score:0},
+	                {name:"老唐",level:"9.47",achievement:7,
+						winPercent:"55%",club:"JP",score:0}];
+
+function goHome(){
+	window.open("gameMenu.html","_self");
+}
+
